6700ef70d2bfb8b52f14a97e4b07bb55b18c7668
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2022-03-31 15:52:50 +0100

    Update ps scripts

--- full-rebuilds/windows-msvc16-x86_64.ps1
+++ full-rebuilds/windows-msvc16-x86_64.ps1
@@ -1,3 +1,5 @@
+# Time this with Measure-Command -Expression { .\conan-ccdc-index\full-rebuilds\windows-msvc16-x86_64.ps1 > .\rebuildall.log 2>&1 }
+# in a powershell with administrative access
 $WS=Resolve-Path -Path "$($PSScriptRoot)/../.."
 
 # This presumes you have a python virtualenv in citvenv with conan-ccdc-index-tools installed
@@ -6,10 +8,13 @@ $WS=Resolve-Path -Path "$($PSScriptRoot)/../.."
 # choco install -y cmake.install --installargs 'ADD_CMAKE_TO_PATH=System'
 # choco install -y python2
 
+Write-Host "If the meson build fails, you may have to enable Developer Mode. See https://github.com/conan-io/conan/issues/10726"
 $CONAN_USER="cbantaloukas"
 $Env:LOCAL_CONAN_INDEX="$($WS)/conan-ccdc-index"
 $Env:LCI_PLATFORM_COMBINATION="native-windows-msvc16-x86_64"
-Remove-Item -recurse -force "$($HOME)/.conan" -ErrorAction SilentlyContinue
+$Env:CONAN_USER_HOME="E:\conan-home-msvc16"
+
+Remove-Item -recurse -force $Env:CONAN_USER_HOME -ErrorAction SilentlyContinue
 conan config install "$($WS)/conan-ccdc-common-configuration"
 
 conan user --remote ccdc-3rdparty-conan-testing --password=$Env:ARTIFACTORY_API_KEY $CONAN_USER
