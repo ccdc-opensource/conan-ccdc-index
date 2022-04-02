94034bee072bd65e8ef79257045d7f0d6cb9fac1
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2022-04-03 00:36:40 +0100

    Fix cmakedeps in openscenegraph

--- recipes/openscenegraph/all/test_package/conanfile.py
+++ recipes/openscenegraph/all/test_package/conanfile.py
@@ -1,25 +1,32 @@
-from conans import CMake, ConanFile, tools
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        for key, value in self.options["openscenegraph"].items():
+            if key.startswith("with_"):
+                tc.variables["OSG_HAS_" + key.upper()] = 1 if value else 0
+        if self.settings.os == "Macos":
+            tc.variables["OSG_HAS_WITH_GIF"] = 0
+            tc.variables["OSG_HAS_WITH_JPEG"] = 0
+            tc.variables["OSG_HAS_WITH_PNG"] = 0
+
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        for key, value in self.options["openscenegraph"].items():
-            if key.startswith("with_"):
-                cmake.definitions["OSG_HAS_" + key.upper()] = 1 if value else 0
-        if self.settings.os == "Macos":
-            cmake.definitions["OSG_HAS_WITH_GIF"] = 0
-            cmake.definitions["OSG_HAS_WITH_JPEG"] = 0
-            cmake.definitions["OSG_HAS_WITH_PNG"] = 0
-
         cmake.configure()
         cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
+            bin_path = os.path.join(self.build_folder, "test_package")
             self.run(bin_path, run_environment=True)
